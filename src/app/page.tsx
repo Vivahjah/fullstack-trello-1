d37cae4081eb873403bc7edbd850c7badc493f94
Mengwa Ugo <mengwa44@yahoo.com>
2025-09-29 01:24:12 +0100

    fixing layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,27 @@
-import Board from "@/components/board";
+
 import LoginViews from "@/components/views/loginViews";
 import { authOptions } from "@/lib/authOptions";
 import { getServerSession } from "next-auth";
+import Link from "next/link";
+
 
 export default async function Home() {
 
   const session = await getServerSession(authOptions);
-  
-  console.log(session, "session");
-  if(!session){
+
+  if (!session) {
     return (
       <LoginViews />
     )
   }
   return (
-  <div>
-    <div className="text-4xl">
-      Your Board
+    <div> 
+      <div className="text-4xl mb-4">Your boards</div>
+      <div className="">
+        <Link className="btn primary inline-block"
+        href={"/new-board"}>Create new board
+        </Link>
+      </div>
     </div>
-    <p>goes here ...</p>
-    {/* <Board /> */}
-  </div>
   )
 }
